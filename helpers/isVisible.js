8dd4da53bc3c1c040c0da0b468cf944c7983da49
pfiel <felek.000@gmail.com>
2022-01-30 18:55:47 +0100

    refactor()

--- helpers/isVisible.js
+++ helpers/isVisible.js
@@ -5,13 +5,25 @@
  * @returns {boolean}
  */
 const isVisible = (formData, visibility) => {
-    const myVisibility = visibility.split('===');
-    const propName = myVisibility[0].trim()
+    const visibilityArray = visibility.split(' ');
+    const propName = visibilityArray[0].trim();
     if (propName === 'always') return true
-    else if (formData[propName] !== undefined) {
-        return myVisibility[1].trim().toLowerCase() === formData[propName].trim().toLowerCase();
+    const condition = visibilityArray[1].trim().toLowerCase();
+    const value = visibilityArray[2].trim().toLowerCase();
+    if (formData[propName] !== undefined) {
+        /**
+         * @TODO bad idea
+         */
+        return eval(`'${value}' ${condition} '${formData[propName].trim().toLowerCase()}'`);
     }
     return false;
+    // const myVisibility = visibility.split('===');
+    // const propName = myVisibility[0].trim()
+    // if (propName === 'always') return true
+    // else if (formData[propName] !== undefined) {
+    //     return myVisibility[1].trim().toLowerCase() === formData[propName].trim().toLowerCase();
+    // }
+    // return false;
 }
 
 export default isVisible;
